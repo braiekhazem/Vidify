e21eb47e39cdd88db2714a96e2e625353972b4e3
braiekhazem <braiekhazem@takiacademyteam.com>
2024-04-28 21:32:07 +0100

    fix: package deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import svgr from "vite-plugin-svgr";
 import path from "path";
 import dts from "vite-plugin-dts";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
-import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +17,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: "vidify",
       fileName: (format) => `index.${format}.js`,
     },
